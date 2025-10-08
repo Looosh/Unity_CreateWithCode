69c025b1a23282f9956e67acbbeba911dfd484cd
Lucius Manzi <manzi.l@northeastern.edu>
2025-10-08 13:46:06 -0400

    Prototype 1 project

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get user input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotate the vehicle based on horixontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+}
