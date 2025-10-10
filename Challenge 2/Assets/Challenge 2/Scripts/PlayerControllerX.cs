8299215f5d302d219829192084d440fd6596896b
Lucius Manzi <manzi.l@northeastern.edu>
2025-10-09 20:37:50 -0400

    Challenge 2 complete

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeSinceLastSpawn = 0;
+    private float spawnCooldown = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastSpawn >= spawnCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceLastSpawn = 0;
+        }
+    }
+}
